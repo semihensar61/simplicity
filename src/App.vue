f1ddd7cc6ca94b9ae7b21a63d611d99749ab9019
Semih Bahadır <semihbahadir@KAVAK347.local>
2022-05-30 13:22:25 +0300

    login component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+<header class="header">
+  <div class="app-name">Blogr</div>
+    <div v-if="authStatus" id="nav">
+      <div>{{user.name}}</div>
+      <button class="auth-button" @click="logOut" > Log Out</button>
+    </div>
+</header>
 </template>
 
 <style>
