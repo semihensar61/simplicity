8108a03183088a428f4d83018ab87d221e730aba
Semih Bahadır <semihbahadir@KAVAK347.local>
2022-05-30 15:44:21 +0300

    user login state added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { apolloClient } from "@/vue-apollo";
+import { LOGGED_IN_USER } from "@/graphql/queries";
+import { LOGIN_USER, REGISTER_USER } from "@/graphql/mutations";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    token: null,
+    user: {},
+    authStatus: false,
+  },
   getters: {},
   mutations: {},
   actions: {},
