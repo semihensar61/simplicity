ad8c9a9082d0178c1d009247738238c233ce1622
Semih Bahadır <semihbahadir@KAVAK347.local>
2022-05-30 16:05:28 +0300

    general css file created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
+<template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <main>
+      authed
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+import { mapGetters } from 'vuex'
 export default {
-  name: "HomeView",
+  name: 'Home',
   components: {
-    HelloWorld,
   },
-};
+  computed: {
+
+  },
+    computed: {
+    ...mapGetters(['user'])
+  }
+}
 </script>
