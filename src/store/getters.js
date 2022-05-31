068faf7de9e725f785ee6ca0b35b263d1df82d33
Semih Bahadır <semihbahadir@KAVAK347.local>
2022-05-31 12:31:41 +0300

    getters added to store

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,4 +2,6 @@ export default {
     isAuthenticated: (state) => !!state.token,
     authStatus: (state) => state.authStatus,
     user: (state) => state.user,
+    restaurants: (state) => state.restaurants,
+    orders: (state) => state.orders
 }
