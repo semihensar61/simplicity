05bcbf80f55b2eba74c52794b4a3324f29c3a9e9
Semih Bahadır <semihbahadir@KAVAK347.local>
2022-05-31 12:37:55 +0300

    empty table component created

---
+++ src/components/table.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <main>authed</main>
+    <div>{{this.orders}}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Home",
+  props:{
+    tableArray:Array
+  },
+  computed: {
+  },
+  mounted() {
+  }
+};
+</script>
